67a978a684fa1db3ec5336d2d3a47d186ccfbc12
Nicolas Wulk <nicolas.wulk@gmail.com>
2024-04-01 18:05:05 -0300

    Adicionando classe de contexto

---
+++ src/todoz.api/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ampare.api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        // Construtor
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet para a tabela de cadastros
+        public DbSet<Cadastro> Cadastros { get; set; }
+    }
+  
+}
