6b76f11e9ea8f0bbc20fd9b64995e92cc942caed
Nicolas Wulk <nicolas.wulk@gmail.com>
2024-04-01 20:55:40 -0300

    Criando service para busca externa de CNPJ

--- src/todoz.api/Program.cs
+++ src/todoz.api/Program.cs
@@ -1,4 +1,5 @@
 using ampare.api.Models;
+using ampare.api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure Cnpj service
+builder.Services.AddHttpClient<CnpjService>(client =>
+{
+    client.BaseAddress = new Uri("https://www.receitaws.com.br/v1/");
+});
 
 // Configure the database
 builder.Services.AddDbContext<AppDbContext>(options =>
