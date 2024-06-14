24403414e4ef4d22f58014b30919b1332f13d134
Michele Freitas <mmichelefreitas@gmail.com>
2024-06-14 10:33:37 -0300

    Pequenos ajustes no App e Cadastro de voluntários

--- src/ampare-webapp/src/App.jsx
+++ src/ampare-webapp/src/App.jsx
@@ -3,9 +3,11 @@ import { Footer } from "./componets/Footer";
 import { Header } from "./componets/Header";
 import { OngForm } from "./pages/OngForm";
 import { ProjetoVoluntario } from "./pages/ProjetoVoluntario";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./componets/Home";
 import { ProjetoForm } from "./pages/ProjetoForm";
+import { VoluntarioForm } from "./pages/VoluntarioForm";
+
 
 function App() {
   return (
@@ -16,7 +18,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/cadastro-ong" element={<OngForm />} />
-          {/* <Route path="/cadastro-voluntario" element={<VoluntarioForm />} /> */}
+          <Route path="/cadastro-voluntario" element={<VoluntarioForm />} />
           <Route path="/cadastro-projeto" element={<ProjetoForm />} />
           <Route path="/listagem-projetos" element={<ProjetoVoluntario />} />
 
