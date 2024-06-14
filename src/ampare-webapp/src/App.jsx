b0743b9b834853e0bc21828c8640747b0f55e948
Matheus-9 <matheusexu@gmail.com>
2024-06-14 09:33:48 -0300

    Resolve merge conflictn

--- src/ampare-webapp/src/App.jsx
+++ src/ampare-webapp/src/App.jsx
@@ -3,6 +3,7 @@ import { Footer } from "./componets/Footer";
 import { Header } from "./componets/Header";
 import { ProjetoForm } from "./pages/ProjetoForm";
 import VisualizarONGs from "./pages/VisualizarONGs";
+import CastroDeVoluntarios from "./pages/CadastroDeVoluntarios";
 
 function App() {
   return (
